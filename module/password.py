50eaca09140ee7692e6df81dea079628715f5bba
hotamago <nhson2003@outlook.com>
2024-01-14 22:00:20 +0700

    fix hash byte

--- module/password.py
+++ module/password.py
@@ -4,4 +4,4 @@ def hash_password(password):
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 def check_password(password, hashed):
-    return bcrypt.checkpw(password.encode('utf-8'), hashed)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed.encode('utf-8'))
